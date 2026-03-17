03cdd0efe4e6e164e74f6a997112866ba41feb9c
DrMineword <165913854+DrMineword@users.noreply.github.com>
2026-03-17 17:20:43 +0100

    Update mapgen.animate.js

--- build/scripts/map/mapgen.animate.js
+++ build/scripts/map/mapgen.animate.js
@@ -32,11 +32,11 @@ MapGen.animate = function (time = performance.now()) {
 });
 
 
-try {
-  MapGen._resolveLabelOverlap();
-} catch (err) {
-  console.warn("[MapGen] label overlap resolver failed", err);
-}
+//try {
+//  MapGen._resolveLabelOverlap();
+//} catch (err) {
+  //console.warn("[MapGen] label overlap resolver failed", err);
+//}
    
 const delta = (time - MapGen._lastTime) / 1000;
 MapGen._lastTime = time;
