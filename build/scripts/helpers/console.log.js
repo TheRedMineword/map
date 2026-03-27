4d925dffdd6e18e1957b0887a739c048e269915f
DrMineword <165913854+DrMineword@users.noreply.github.com>
2026-03-27 13:44:57 +0100

    Delete build directory

--- build/scripts/helpers/console.log.js
+++
@@ -1,7 +0,0 @@
-console.log("[HELPER] Console logger loading");
-
-MapGen.DEBUG = new URLSearchParams(location.search).get("debug") === "true";
-
-MapGen.log = function (...args) {
-  if (MapGen.DEBUG) console.log(...args);
-};
--- build/scripts/map/logscene.js
+++
@@ -1,3 +0,0 @@
-console.log("SCENE HERE INFO");
-console.log(MapGen.scene); // returns your THREE.Scene object
-console.log("SCENE HERE INFO");
